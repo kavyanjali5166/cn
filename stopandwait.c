43b460e3c7b25689166cc13ce639b1fd61efee8e
kavyanjali5166 <152371632+kavyanjali5166@users.noreply.github.com>
2025-11-28 06:16:49 +0530

    Update stopandwait.c

--- stopandwait.c
+++ stopandwait.c
@@ -1,39 +1,29 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<time.h>
-void sender(inttotalFrames);
-void receiver(int frame);
-void main()
-{
-inttotalFrames;
-	printf("=== Stop and wait protocol Simulation ===\n);
-	printf("Enter total no.of frames to send: ");
-	scanf("%d", &totalFrames);
-	sender(totalFrames);
-}
-void sender(inttotalFrames)
-{
-	inti,ack,choice;
-	srand(time(NULL));
-	for(i=1;i<=totalFrames;i++)
-	{
-		printf("\nSender: Sending Frame %d",i);
-		printf("\nReceiver: Do you want to ACK Frame %d? (1=Yes,0=No): ",i);
-		scanf("%d",&choice);
-		if(choice==1)
-		{
-receiver(i);
-			printf("Sender: ACK %d received \n",i);
-		}
-		else
-		{
-			printf("Sender: ACK %d lost.Retransmitting...\n",i);
-i--
-		}
-	}
-	printf("\nAll %d frames sent suceesfully!\n",totalFrames);
-}
-void receiver(int frame)
-{
-	printf("Reciever: Frame %d received.Sending ACK %d...\n,frame,frame);
+#include <stdio.h>
+
+int main() {
+    int total_frames, ack, i;
+
+    printf("Enter total number of frames to send: ");
+    scanf("%d", &total_frames);
+
+    printf("\n--- Stop and Wait ARQ Simulation ---\n");
+
+    for (i = 1; i <= total_frames; i++) {
+        printf("\nSending frame %d...", i);
+
+        // Check if frame received correctly
+        printf("\nDid you receive ACK for frame %d? (1 = Yes, 0 = No): ", i);
+        scanf("%d", &ack);
+
+        if (ack == 1) {
+            printf("Frame %d acknowledged successfully.\n", i);
+        } else {
+            printf("Timeout! Frame %d not acknowledged.\n", i);
+            printf("Resending frame %d...\n", i);
+            printf("Frame %d received successfully now.\n", i);
+        }
+    }
+
+    printf("\nAll frames sent and acknowledged successfully.\n");
+    return 0;
 }
