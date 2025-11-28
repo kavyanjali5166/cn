763ae36b41864d0f3910271e45ac2e12c221ccb8
kavyanjali5166 <152371632+kavyanjali5166@users.noreply.github.com>
2025-11-28 06:15:19 +0530

    Update selectiverepeat.c

--- selectiverepeat.c
+++ selectiverepeat.c
@@ -1,56 +1,42 @@
-#include<stdio.h> 
-#include<stdlib.h> 
-#define MAX 50 
-void main() 
-{ 
-int totalFrames,windowSize; 
-int frameStatus[MAX]; 
-int i,choice,resend; 
-printf("\n===Selective Repeat ARQ Simulation ===\n"); 
-printf("Enter total number of frames to send: ");
-scanf("%d",&totalFrames); 
-printf("Enter window size: "); 
-scanf("%d",&windowSize); 
-for(i=0;i<totalFrames;i++) 
-frameStatus[i]=0; 
-int sent=0; 
-while(sent<totalFrames) 
-{ 
-printf("\n--- Sending Window of Frames ---\n"); 
-for(i=sent;i<sent+windowSize && i<totalFrames;i++) 
-{ 
-if(frameStatus[i]==0) 
-printf("Frame %d sent.\n",i); 
-} 
-for(i=sent;i<sent+windowSize && i<totalFrames;i++) 
-{ 
-if(frameStatus[i]==0) 
-{ 
-printf("Did frame %d get ACK? (1=yes,0=lost): ",i); 
-scanf("%d",&choice); 
-if(choice == 1) 
-{ 
-printf("ACK for frame %d received.\n",i); 
-frameStatus[i]=1; 
-} 
-else 
-printf("Frame %d lost / not acknowledge.\n",i); 
-} 
-} 
-printf("\n DO you want to resend any lost frame?(Enter frame number,-1 to stop):"); scanf("%d",&resend); 
-while(resend!=-1) 
-{ 
-if(resend>=sent && resend<totalFrames && frameStatus[resend]==0) { 
-printf("Resending frame %d...\n",resend);
-printf("ACK for frame %d received now.\n",resend); 
-frameStatus[resend]=1; 
-} 
-else 
-printf("Invalid frame number or already ACKed.\n"); 
-printf("Enter next frame to resend (-1 to stop):"); 
-scanf("%d",&resend); 
-} 
-while(sent<totalFrames && frameStatus[sent]==1) 
-sent++; 
-} 
-printf("\nAll frames sent and acknowledged successfully!\n"); } 
+#include <stdio.h>
+
+int main() {
+    int total_frames, window_size;
+    int i = 1, j, ack;
+
+    printf("Enter total number of frames to send: ");
+    scanf("%d", &total_frames);
+
+    printf("Enter window size: ");
+    scanf("%d", &window_size);
+
+    printf("\n--- Selective Repeat ARQ Simulation ---\n");
+
+    while (i <= total_frames) {
+        // Send frames in the current window
+        printf("\nSending frames: ");
+        for (j = i; j < i + window_size && j <= total_frames; j++) {
+            printf("%d ", j);
+        }
+
+        // Acknowledgment checking
+        for (j = i; j < i + window_size && j <= total_frames; j++) {
+            printf("\nDid you receive ACK for frame %d? (1 = Yes, 0 = No): ", j);
+            scanf("%d", &ack);
+
+            if (ack == 0) {
+                printf("Resending frame %d...\n", j);
+                printf("Frame %d received successfully now.\n", j);
+            } else {
+                printf("Frame %d acknowledged successfully.\n", j);
+            }
+        }
+
+        // Move to next set of frames
+        i = i + window_size;
+    }
+
+    printf("\nAll frames sent and acknowledged successfully.\n");
+    return 0;
+}
+
